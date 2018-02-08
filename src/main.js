6f8c70dd2f0ae4b2b90ed564b898042c0cabb1bf
Christopher Briggs <confusedmatrix@gmail.com>
2018-02-08 10:44:50 +0000

    Added card randomise and flip functions

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import { Store } from 'svelte/store'
 import App from './templates/App.html'
 import cards from './cards.json'
 
-const store = new Store({
+class AppStore extends Store {
+	loadRandomCard(currentCardId) {
+		const cards = this.get('cards').filter(card => card.id !== currentCardId)
+        const card = cards[Math.floor(Math.random() * cards.length)]
+        this.set({
+			currentCard: card.id,			
+		})
+	}
+}
+
+const store = new AppStore({
 	cards,
 	currentCard: 'socrative',
 })
