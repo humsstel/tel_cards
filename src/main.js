43b5dcca580a11fa324bb7c9e7f5473879d95e1d
Christopher Briggs <confusedmatrix@gmail.com>
2018-02-09 11:30:46 +0000

    Added about, catalogue and contribute tab content

--- src/main.js
+++ src/main.js
@@ -13,6 +13,19 @@ class AppStore extends Store {
 	}
 }
 
+cards.forEach(card => {
+	const activities = [
+		{ name: "create", description: "Build learning resources, from scratch or remixing existing materials." },
+		{ name: "captivate", description: "Create interactive learning opportunities that engage." },
+		{ name: "check", description: "Gauge students understanding." },
+		{ name: "connect", description: "Communicate with students in real time or asynchronously." },
+		{ name: "collaborate", description: "A platform for collaboration and co-creation." },
+		{ name: "curate", description: "Collect, organise and share content." },
+		{ name: "capture", description: "Record an event, activity or artefact." },
+	]
+	card.activities = card.activities.map(name => activities.find(activity => activity.name === name))
+})
+
 const store = new AppStore({
 	cards,
 	currentCard: 'socrative',
