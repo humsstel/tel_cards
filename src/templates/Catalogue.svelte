84800a74e82c629907265762be4356fd5a032698
Christopher Briggs <confusedmatrix@gmail.com>
2018-03-01 11:39:04 +0000

    Added svelte config file and renames svelte templates

---
+++ src/templates/Catalogue.svelte
@@ -0,0 +1,44 @@
+<div class="card-container manual-flip">
+    <div class="col-md-12">
+        <div class="bs-callout bs-callout-catalogue clearfix">
+            <div class="icon">
+                <i class="fa fa-bookmark-o" aria-hidden="true"></i> 
+            </div>
+            <h4>Catalogue</h4>
+            <p>Browse the list or use the dropdown filter to find an application that supports a specific activity.</p>
+        </div>
+        <div class="dropdown">
+            <select id="filterText" class="btn btn-secondary dropdown-toggle btn-block" onchange='filterText();'>
+                <option selected value="all">Find an app to...</option>
+                <option value="Create">...create learning resources</option>
+                <option value="Connect">...communicate with students electronically</option>
+                <option value="Collaborate">...act as a platform for collaboration</option>
+                <option value="Curate">...collect, organise and share content</option>
+                <option value="Capture">...record an event or artefact</option>
+                <option value="Captivate">...create interactive learning opportunities</option>
+                <option value="Check">...gauge students understanding</option>
+                <option value="">All apps</option>
+            </select>
+        </div>
+        <div class="card cataloguepane" data-spy="scroll">
+            <div class="front">
+                <ul class="list-group list-group-flush">
+                    {{#each $cards as card}}
+                    <li class="list-group-item list-group-item-action">
+                        <div class="applist">
+                            <h5><img style="display: inline; height: 1em; vertical-align: top;" src="img/{{ card.image.logo }}"> {{ card.name }}</h5>
+                            <p class="categories">
+                                {{#each card.activities as activity}}
+                                <span class="badge activity {{ activity.name }}">{{ activity.name }}</span>&nbsp;
+                                {{/each}}
+                            </p>
+                            <p class="submitter"><small>Submitted by {{ card.submitter.name }} | {{ card.submitter.title }}</small></p>
+                            <p class="appinfo">{{ card.tagline }}</p>
+                        </div>
+                    </li>
+                    {{/each}}
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
