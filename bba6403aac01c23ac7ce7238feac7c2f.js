8b35e588b8c3bd9ec4188542821e0ea93b4bb478
Christopher Briggs <confusedmatrix@gmail.com>
2018-06-05 11:16:08 +0100

    publish: Added scrollbar to sidebar
    generated from commit 610e3f76a6028077f66fc30dc4f04daa07d4a50c

--- bba6403aac01c23ac7ce7238feac7c2f.js
+++ bba6403aac01c23ac7ce7238feac7c2f.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({13:[function(require,module,exports) {
+})({14:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],7:[function(require,module,exports) {
+},{}],8:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":13}],3:[function(require,module,exports) {
+},{"./bundle-url":14}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":7}],43:[function(require,module,exports) {
+},{"_css_loader":8}],44:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60451' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '61050' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -261,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[43])
+},{}]},{},[44])
 //# sourceMappingURL=/dist/bba6403aac01c23ac7ce7238feac7c2f.map
