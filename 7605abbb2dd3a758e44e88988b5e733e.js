f6828a0b2ef93922390d1dede496b3bc8486266e
danieladamharding <danieladamharding@gmail.com>
2018-08-15 14:28:52 +0100

    publish: Removed wall container to avoid height issues
    generated from commit b95a99a7abcb7a732780413ab4010537f05b52b8

--- 7605abbb2dd3a758e44e88988b5e733e.js
+++ 7605abbb2dd3a758e44e88988b5e733e.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({8:[function(require,module,exports) {
+})({12:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],6:[function(require,module,exports) {
+},{}],7:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":8}],3:[function(require,module,exports) {
+},{"./bundle-url":12}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":6}],41:[function(require,module,exports) {
+},{"_css_loader":7}],49:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50023' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '56206' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -261,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[41])
+},{}]},{},[49])
 //# sourceMappingURL=/dist/7605abbb2dd3a758e44e88988b5e733e.map
