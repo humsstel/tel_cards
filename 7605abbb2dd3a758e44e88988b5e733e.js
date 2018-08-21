a8325163dc86f229b5a18060638b24b0f6f7fd93
danieladamharding <danieladamharding@gmail.com>
2018-08-21 16:38:13 +0100

    publish: Removed wall iframe margin and fixed Support tabs
    generated from commit 8dc796673e090a4b7cd90f8e5569bc705a583bea

--- 7605abbb2dd3a758e44e88988b5e733e.js
+++ 7605abbb2dd3a758e44e88988b5e733e.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({8:[function(require,module,exports) {
+})({11:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -132,7 +132,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":8}],3:[function(require,module,exports) {
+},{"./bundle-url":11}],4:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50830' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59321' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
