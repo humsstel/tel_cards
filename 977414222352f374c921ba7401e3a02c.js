3e8e6ecea95e8c2c2d91fb1c87c12283d4f03928
humsstel <humss.lto@keele.ac.uk>
2019-01-14 17:07:14 +0000

    publish: iframe adjustment
    generated from commit ec0f05b34b5caf473482d58df5909f9b97192274

--- 977414222352f374c921ba7401e3a02c.js
+++ 977414222352f374c921ba7401e3a02c.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({11:[function(require,module,exports) {
+})({12:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],6:[function(require,module,exports) {
+},{}],7:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":11}],3:[function(require,module,exports) {
+},{"./bundle-url":12}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./../img/icons/down-arrow-white.png":7,"_css_loader":6}],43:[function(require,module,exports) {
+},{"./../img/icons/down-arrow-white.png":11,"_css_loader":7}],44:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54189' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '56578' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -261,7 +261,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}],45:[function(require,module,exports) {
+},{}],46:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -340,7 +340,7 @@ LazyPromise.prototype.then = function (onSuccess, onError) {
 LazyPromise.prototype.catch = function (onError) {
   return this.promise || (this.promise = new Promise(this.executor).catch(onError));
 };
-},{"./bundle-url":11}],0:[function(require,module,exports) {
-var b=require(45);b.load([["052eb4003b254416ede8fe35a4cbd9b3.png",7]]);
-},{}]},{},[43,0])
+},{"./bundle-url":12}],0:[function(require,module,exports) {
+var b=require(46);b.load([["052eb4003b254416ede8fe35a4cbd9b3.png",11]]);
+},{}]},{},[44,0])
 //# sourceMappingURL=/dist/977414222352f374c921ba7401e3a02c.map
